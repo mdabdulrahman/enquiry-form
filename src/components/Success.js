c914f93a296cb959f2a606d1b11c1511d1ec7cd3
Abdul Rahman <abdulrahmanasus@outlook.com>
2022-10-16 11:10:13 +0530

    mobile

--- src/components/Success.js
+++ src/components/Success.js
@@ -6,8 +6,8 @@ function Success() {
   return (
     <div>
         <div className='w-5/6 mx-auto  lg:w-full text-center my-20'>
-<h1 className='flex justify-center items-center lg:gap-5 '>
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 text-green-500 h-8">
+<h1 className='lg:flex justify-center items-center lg:gap-5 '>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-11 mx-auto lg:mx-0 text-green-500 h-11">
   <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 </svg>
 
